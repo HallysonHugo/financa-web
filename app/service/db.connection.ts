741505bd0d41419d0284f485d6f374623cf310e4
hallyson <hallyson_hugo@hotmail.com>
2024-11-01 13:50:01 -0300

    add mongo connection as variable

--- app/service/db.connection.ts
+++ app/service/db.connection.ts
@@ -4,7 +4,11 @@ let connection: Mongoose | null = null;
 
 async function connectMongo() {
     if (connection) return connection;
-    connection = await connect('mongodb+srv://hallyson:6j74U0e1JsnFP27h@cluster0.fpptiy1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    const mongoUrl = process.env.MONGO_URL;
+    if (!mongoUrl) {
+        throw new Error("MongoDB URL not found");
+    }
+    connection = await connect(mongoUrl, {
         serverSelectionTimeoutMS: 10000, // 10 seconds timeout
         socketTimeoutMS: 45000, // 45 seconds socket timeout
     });
